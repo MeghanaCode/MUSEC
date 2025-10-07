f7bc2a5de0a563c3f65f5cab852edb8667440c3e
Meghana Alugunoolla <meghanaalugunoolla@Meghanas-MacBook-Pro.local>
2025-10-07 14:31:07 +0200

    Add MAX-binary-2.0.1 executable

---
+++ MAX-binary-2.0.1/configure_pre.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+##### Date:04 June 2020
+##### configure.sh: set the current path for loading R functions
+istr="/path/to"
+ostr="$PWD"
+eval "sed -i -e 's#"$istr"#"$ostr"#g' R/AEM_update_X_beta.R"
+eval "sed -i -e 's#"$istr"#"$ostr"#g' R/Create_count_matrix.R"
+eval "sed -i -e 's#"$istr"#"$ostr"#g' R/buildCRP.R"
+eval "sed -i -e 's#"$istr"#"$ostr"#g' R/mergeMutSingleSample.R"
+eval "sed -i -e 's#"$istr"#"$ostr"#g' R/estimateBeta_MAX2.R"
+eval "sed -i -e 's#"$istr"#"$ostr"#g' R/genCountSample.R"
+eval "sed -i -e 's#"$istr"#"$ostr"#g' R/splitFasta.R"
+eval "sed -i -e 's#"$istr"#"$ostr"#g' new_constructMutXmatrix.sh"
+eval "sed -i -e 's#"$istr"#"$ostr"#g' runMAX_step3_createYcount.sh"
+
+echo "Done!"
